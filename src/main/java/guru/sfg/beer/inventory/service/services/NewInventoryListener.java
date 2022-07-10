3d7ed20891143e27924657a1c9de73afefdeade1
zy51927042 <zy51927042@163.com>
2022-07-10 16:29:33 +0800

    refactor to common model.Closes #2

--- src/main/java/guru/sfg/beer/inventory/service/services/NewInventoryListener.java
+++ src/main/java/guru/sfg/beer/inventory/service/services/NewInventoryListener.java
@@ -2,7 +2,7 @@ package guru.sfg.beer.inventory.service.services;
 
 import guru.sfg.beer.inventory.service.config.JmsConfig;
 import guru.sfg.beer.inventory.service.domain.BeerInventory;
-import guru.sfg.common.events.NewInventoryEvent;
+import guru.sfg.brewery.model.events.NewInventoryEvent;
 import guru.sfg.beer.inventory.service.repositories.BeerInventoryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
